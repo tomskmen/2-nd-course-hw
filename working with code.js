22bfd35835033d93ad2f7f5b3dd3f79752516588
tomskmen <tomskmen@gmail.com>
2025-02-19 20:29:50 +0300

    Решение задачи5

--- working with code.js
+++ working with code.js
@@ -37,4 +37,42 @@ let b = '3';
 alert(Number(a) + Number(b)); 
 
 
+let monthNumber = Number(prompt("Введите номер месяца (от 1 до 12):")); // Запрашиваем ввод у пользователя
+
+if (monthNumber < 1 || monthNumber > 12) {
+    console.log("Неверно");
+    alert("Неверно: в году только 12 месяцев!");
+} else {
+    switch (monthNumber) {
+        case 12:
+        case 1:
+        case 2:
+            console.log("Зима");
+            alert("Зима");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log("Весна");
+            alert("Весна");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log("Лето");
+            alert("Лето");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            console.log("Осень");
+            alert("Осень");
+            break;
+    }
+    console.log("Верно");
+    alert("Верно");
+}
+
+
+
 
